c091ea51c52439e627e05f36b08c13ae7f3eed48
ROJAS MARTINEZ <s16120309@alumnos.itsur.edu.mx>
2021-02-11 17:19:39 -0600

    Se creo la interfaz de agregar contacto

---
+++ app/src/main/java/Fragments/InsertarFragmet.java
@@ -0,0 +1,25 @@
+package Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.contentprovidercliente.R;
+
+public class InsertarFragmet extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.insertar_fragmet,container,false);
+
+
+        return view;
+    }
+
+}
